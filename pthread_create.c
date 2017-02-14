da793603d5fe94cfa1c734ec81c7aa50bd698c71
itachigorun <itachigorun@gmail.com>
2017-02-14 23:17:41 +0800

    createPthread

---
+++ pthread_create.c
@@ -0,0 +1,27 @@
+/* 2017-02-14  */
+/* create thread,gcc -o test pthread_create.c pthread  */
+#include <stdio.h>
+#include <pthread.h>
+
+int * thread(void *arg)
+{
+    printf("thread id is %u .\n",(unsigned int)pthread_self());
+    return NULL;
+}
+
+int main()
+{
+    pthread_t id;
+    printf("Main thread id is %u \n",(unsigned int)pthread_self());
+    
+    if(!pthread_create(&id,NULL,(void *)thread,NULL))
+    {
+	printf("succeed!\n");
+	return 0;
+    }
+    else
+    {
+        printf("Fail to Create Thread");
+	return -1;
+    }
+}
