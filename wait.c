42e16c1415c02b4d2c5f82d905cc4f11e22f7dd6
itachigorun <itachigorun@gmail.com>
2017-03-08 00:26:04 +0800

    ..

--- wait.c
+++ wait.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<sys/wait.h>
-#include<unistd.h>
 #include<stdlib.h>
 
 void pr_exit(int status)
@@ -8,14 +7,12 @@ void pr_exit(int status)
     if(WIFEXITED(status))
         printf("normal termination, exit status = %d\n",WEXITSTATUS(status));
     else if (WIFSIGNALED(status))
-        printf("abnormal termination, signal number = %d%s\n", WTERMSIG(status));
-/*
+        printf("abnormal termination, signal number = %d%s\n", WTERMSIG(status),
 #ifdef WCOREDUMP
                WCOREDUMP(status) ? " (core file generated)" : "");
 #else
                "");
 #endif
-*/
 
     else if (WIFSTOPPED(status))
         printf("child stopped, signal number = %d\n", WSTOPSIG(status));
@@ -33,7 +30,7 @@ int main()
 
     if(wait(&status) != pid)      /* wait for child */
         printf("wait error");
-    pre_exit(status);             /* and print its status */
+    pr_exit(status);             /* and print its status */
 
     if((pid = fork()) < 0)
         printf("fork error");
@@ -42,16 +39,16 @@ int main()
 
     if(wait(&status) != pid)
         printf("wait error");
-    pre_exit(status);
+    pr_exit(status);
 
     if((pid = fork()) < 0)
         printf("fork error");
     else if(pid == 0)
-        status /= 0;
+ //      status /= 0;
 
     if(wait(&status) != pid)
         printf("wait error");
-    pre_exit(status);
+    pr_exit(status);
 
 return 0;
 }
