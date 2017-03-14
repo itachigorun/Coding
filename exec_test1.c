55da9bc0b775fbd078818e6d1a5a5271e357ba94
itachigorun <itachigorun@gmail.com>
2017-03-14 22:51:36 +0800

    exec function

--- exec_test1.c
+++ exec_test1.c
@@ -1,27 +1,36 @@
-/* exec.c */
-    unistdh #include<.>
-    main void(void)
-    {
-         envp char*[]={"PATH=/tmp","USER=lingdxuyan","STATUS=testing",NULL};
-         argv_execv char*[]={"echo","excuted by execv",NULL};
-         argv_execvp char*[]={"echo","executed by execvp",NULL};
-         argv_execve char*[]={"env",NULL};
-         fork 0 if(()==)
-             execl 0 if(("/bin/echo","echo","executed by execl",NULL)<)
-                 perror("Err on execl");
-         fork 0 if(()==)
-             execlp 0 if(("echo","echo","executed by execlp",NULL)<)
-                 perror("Err on execlp");
-         fork 0 if(()==)
-             execle envp 0 if(("/usr/bin/env","env",NULL,)<)
-                 perror("Err on execle");
-         fork 0 if(()==)
-             execv argv_execv 0 if(("/bin/echo",)<)
-                 perror("Err on execv");
-         fork 0 if(()==)
-             execvp argv_execvp 0 if(("echo",)<)
-                 perror("Err on execvp");
-         fork 0 if(()==)
-             execve argv_execve envp 0 if(("/usr/bin/env",,)<)
-                 perror("Err on execve");
-    }
+#include<unistd.h>
+#include<stdio.h>
+
+int main()
+{
+    char * envp[] = {"PATH=/tmp", "USER=itachigorun", "STATUS=testing", NULL};
+    char * argv_execv[] = {"echo", "excuted by execv", NULL};
+    char * argv_execvp[] = {"echo", "executed by execvp", NULL};
+    char * argv_execve[] = {"env", NULL};
+    
+    if(fork() == 0)
+        if(execl("/bin/echo", "echo", "executed by execl", NULL) < 0)
+	    printf("Err on execl");
+
+    if(fork() == 0)
+        if(execlp("echo", "echo", "executed by execlp", NULL) < 0)
+	    printf("Err on execl");
+
+    if(fork() == 0)
+        if(execle("/usr/bin/env", "env", NULL, envp) < 0)
+	    printf("Err on execl");
+    
+    if(fork() == 0)
+        if(execv("/bin/echo",argv_execv) < 0)
+	    printf("Err on execl");
+    
+    if(fork() == 0)
+        if(execvp("echo",argv_execvp) < 0)
+	    printf("Err on execl");
+    
+    if(fork() == 0)
+        if(execve("/usr/bin/env", argv_execve, envp) < 0)
+	    printf("Err on execl");
+
+return 0;
+}
