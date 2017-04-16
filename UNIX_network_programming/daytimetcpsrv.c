783c8c5741e83e3ae8e4899927a1a54c2ed4fbb3
itachigorun <itachigorun@gmail.com>
2017-04-16 23:08:51 +0800

    new file daytimetcpsrv.c

---
+++ UNIX_network_programming/daytimetcpsrv.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<time.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+
+#define SA struct sockaddr
+#define MAXLINE 4096
+
+int main(int argc, char **argv)
+{
+    int listenfd, connfd;
+    struct sockadd_in servaddr;
+    char buff[MAXLINE];
+    time_t ticks;
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(13);
+
+    bind(listenfd, (SA)&servaddr, sizeof(servaddr));
+
+    listen(listenfd, LISTENQ);
+
+    for(;;){
+        connfd = accept(listenfd, (SA*)NULL,NULL);
+
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buf), "%.24s\r\n", ctime(&ticks));
+        write(connfd, buff, strlen(buff));
+
+        close(confd);
+    }
+
+    return 0;
+}
