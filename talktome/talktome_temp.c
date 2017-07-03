21dcc565102ce37397343f8105a11e017e0ce960
itachigorun <itachigorun@gmail.com>
2017-07-03 23:11:12 +0800

    temp

---
+++ talktome/talktome_temp.c
@@ -0,0 +1,59 @@
+/*
+ *  去掉字符串左空格  
+ */
+char *trimLeftSpace(char *str)
+{
+   char *temp;
+
+   temp = str;
+
+   while (*temp)
+   {
+      if (*temp == ' ')
+         ++temp;
+      else
+         break;
+   }
+   if (temp > str)
+      strcpy (str, temp);
+
+   return(str);
+}
+
+/*
+ *  去掉字符串右空格  
+ */
+char *trimRightSpace(char *str)
+
+    int temp;
+
+    temp = strlen(str);
+    if (temp <= 0)
+        return(str);
+    while ((temp != 0) && (str[temp-1] == ' '))
+        temp--;
+    str[temp]='\0';
+    return(str);
+
+}
+
+/*
+ *  去掉字符串前后空格 
+ */
+char *trimSpace(char *str)
+{
+    return(trimLeftSpace(trimRightSpace(str)));
+}
+
+/*
+ * 将传入的字符串转换为全部转换成大写  
+ */     
+void toUpper(const char *strIn, int len, char *strOut)
+{
+   int i = 0;
+
+   for( i=0; i < len; i++ )
+      strOut[i] = toupper(strIn[i]);
+
+   return;
+}
