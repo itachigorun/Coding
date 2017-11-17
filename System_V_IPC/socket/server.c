91fb5ddd4edc249698a2f7616627a40733a0b38a
itachigorun <itachigorun@gmail.com>
2017-11-17 15:46:10 +0800

    丰富服务端功能

--- System_V_IPC/socket/server.c
+++ System_V_IPC/socket/server.c
@@ -11,12 +11,13 @@
 #define DEFAULT_PORT 8000  
 #define MAXLINE 4096  
 #define FAILURE -1
-int main(int argc, char** argv){  
+int main(int argc, char** argv)
+{  
     int socket_fd, connect_fd;  
-    struct sockaddr_in servaddr, getaddr, clientaddr;  
-    char buff[4096];  
-    int n;
-    int len;
+    struct sockaddr_in servaddr, getcliaddr, getseraddr, clientaddr;  
+    char buff[MAXLINE];  
+    int receivelen;
+    int len = sizeof(struct sockaddr_in);
     int flag = 1;
     struct linger lingopt;
 
@@ -51,8 +52,8 @@ int main(int argc, char** argv){
     //初始化  
     memset(&servaddr, 0, sizeof(servaddr));  
     servaddr.sin_family = AF_INET;  
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); //IP地址设置成INADDR_ANY,让系统自动获取本机的IP地址。  
-    //serveraddr.sin_addr.s_addr = inet_addr(localip);
+    //servaddr.sin_addr.s_addr = htonl(INADDR_ANY); //IP地址设置成INADDR_ANY,让系统自动获取本机的IP地址。  
+    servaddr.sin_addr.s_addr = inet_addr("172.16.1.17");
     servaddr.sin_port = htons(DEFAULT_PORT);      //设置的端口为DEFAULT_PORT  
 
     //将本地地址绑定到所创建的套接字上  
@@ -67,33 +68,54 @@ int main(int argc, char** argv){
         exit(0);  
     }  
 
+    /* 获得本地地址 */
+    if(getsockname(socket_fd, (struct sockaddr *)&getseraddr, (socklen_t *)&len)!=0)
+    {
+        printf("getsockname err\n");
+        close(socket_fd);
+        return FAILURE;
+    }
+    /* 输出5 */
+    memset(buff,0,MAXLINE);
+    strcpy(buff,inet_ntoa(getseraddr.sin_addr));
+    printf("%s\n",buff);
+    
+    /*输出6*/
+    memset(buff,0,MAXLINE);
+    if(inet_ntop(AF_INET, &getseraddr.sin_addr, buff, MAXLINE)==NULL)
+    {
+        printf("error\n");
+        close(socket_fd);
+        return FAILURE;
+    }
+    else
+        printf("%s\n",buff);
+
     printf("======waiting for client's request======\n");  
     while(1){  
-        if(connect_fd = accept(socket_fd, (struct sockaddr*)&clientaddr, (socklen_t *)&sizeof(clientaddr)) == -1)
-	    {  //如果不关心客户端的地址后两个参数填null
-            printf("accept socket error: %s(errno: %d)",strerror(errno),errno);  
+        if((connect_fd = accept(socket_fd, (struct sockaddr*)&clientaddr, (socklen_t *)&len)) == -1) 
+        //connect_fd = accept(socket_fd, (struct sockaddr*)&NULL, NULL); 不关心客户端的地址后两个参数填null
+	    {  
+            printf("accept socket error: %s(errno: %d)",strerror(errno),errno);
+            close(socket_fd);
             return FAILURE;  
-        }  
-        
+        }
+
+        memset(buff,0,MAXLINE);
         //接受客户端传过来的数据  
-        n = recv(connect_fd, buff, MAXLINE, 0);  
+        receivelen = recv(connect_fd, buff, MAXLINE, 0);  
         //向客户端发送回应数据  
         if(!fork()){
             close(socket_fd); //关闭监听socket
+
+            /* 输出1 */
+            memset(buff,0,MAXLINE);
+            strcpy(buff,inet_ntoa(clientaddr.sin_addr));
+            printf("%s\n",buff);
             
-            /*输出1*/
-            memset(buff,0,4096);
-            if(inet_aton(buff, &clientaddr.sin_addr)==0)
-            {
-                printf("error\n");
-                close(connect_fd);
-                return FAILURE;
-            }
-            else
-                printf("%s\n",buff);
             /*输出2*/
-            memset(buff,0,4096);
-            if(inet_ntop(AF_INET, &clientaddr.sin_addr, buff, sizeof(buff))==NULL)
+            memset(buff,0,MAXLINE);
+            if(inet_ntop(AF_INET, &clientaddr.sin_addr, buff, MAXLINE)==NULL)
             {
                 printf("error\n");
                 close(connect_fd);
@@ -101,15 +123,21 @@ int main(int argc, char** argv){
             }
             else
                 printf("%s\n",buff);
-            if(getsockname(connect_fd, (struct sockaddr *)&getaddr, (socklen_t *)&sizeof(getaddr))!=0)
+            /* 获得本地地址 */
+            if(getsockname(connect_fd, (struct sockaddr *)&getcliaddr, (socklen_t *)&len)!=0)
             {
                 printf("getsockname err\n");
                 close(connect_fd);
                 return FAILURE;
             }
-            /*输出3*/
-            memset(buff,0,4096);
-            if(inet_aton(buff, &getaddr.sin_addr) == 0)
+            /* 输出3 */
+            memset(buff,0,MAXLINE);
+            strcpy(buff,inet_ntoa(getcliaddr.sin_addr));
+            printf("%s\n",buff);
+            
+            /*输出4*/
+            memset(buff,0,MAXLINE);
+            if(inet_ntop(AF_INET, &getcliaddr.sin_addr, buff, MAXLINE)==NULL)
             {
                 printf("error\n");
                 close(connect_fd);
@@ -117,32 +145,22 @@ int main(int argc, char** argv){
             }
             else
                 printf("%s\n",buff);
-            /* 输出4 */
-            memset(buff,0,4096);
-            if(inet_ntop(AF_INET, &getaddr.sin_addr, buff, sizeof(buff))==NULL)
-                printf("error\n");
-            else
-                printf("%s\n",buff);
 
-            if(getpeername(connect_fd, (struct sockaddr *)&getaddr, (socklen_t *)&sizeof(getaddr))!=0)
+            /* 获得客户端地址 */
+            if(getpeername(connect_fd, (struct sockaddr *)&getseraddr, (socklen_t *)&len)!=0)
             {
                 printf("getsockname err\n");
                 close(connect_fd);
                 return FAILURE;
             }
             /* 输出5 */
-            memset(buff,0,4096);
-            if((buff = inet_ntoa(getaddr.sin_addr)) == NULL)
-            {
-                printf("error\n");
-                close(connect_fd);
-                return FAILURE;
-            }
-            else
-                printf("%s",buff);
+            memset(buff,0,MAXLINE);
+            strcpy(buff,inet_ntoa(getseraddr.sin_addr));
+            printf("%s\n",buff);
+            
             /*输出6*/
-            memset(buff,0,4096);
-            if(inet_pton(AF_INET, buff, &getaddr.sin_addr) != 1)
+            memset(buff,0,MAXLINE);
+            if(inet_ntop(AF_INET, &getseraddr.sin_addr, buff, MAXLINE)==NULL)
             {
                 printf("error\n");
                 close(connect_fd);
@@ -151,14 +169,12 @@ int main(int argc, char** argv){
             else
                 printf("%s\n",buff);
 
-
-            memset(buff,0,4096);
             if(send(connect_fd, "Hello,you are connected!\n", 26,0) == -1)  
                 perror("send error");  
             close(connect_fd);  
             exit(0);  
             }  
-        buff[n] = '\0';  
+        buff[receivelen] = '\0';  
         printf("recv msg from client: %s\n", buff);  
         close(connect_fd);  
     }
